7e971fcfd9f0b3ba4bb0653d7d57e87445a65435
Droogans <droogans@gmail.com>
2013-01-31 12:43:05 -0500

    test changes; actually run now

--- cryptoquips/tests/create_test.py
+++ cryptoquips/tests/create_test.py
@@ -1,27 +1,33 @@
 #! usr/bin/env python
 
+import unittest
+
 from cryptoquips import create
 
-from unittest import TestCase
+class TestCryptoquip(unittest.TestCase):
 
-class CryptoquipTest(TestCase):
-    setUp(self):
-        self.trans_table = create._trans_table()
+    def setUp(self):
+        self.trans_table = create._random_trans_table()
         self.quip        = create.cryptoquip
         self.words       = create.word_list()
         self.test_quip   = self.quip('this is a test')
 
-    def trans_length(self):
+    def test_trans_length(self):
         self.assertEqual(len(self.trans_table), 256,
                          'create.trans_table returns bad table')
 
-    def crypto_length(self):
+    def test_crypto_length(self):
         self.assertEqual(len(self.test_quip), len(self.quip('what am i zest')),
                          'create.cryptoquip length not equal to quip')
 
-    def crypto_upper(self):
-        self.assertEqual(self.quip('spam'), self.quip('spam').upper(),
-                         'create.cryptoquip does not return upper()')
+    def test_min_replace(self):
+        self.assertEqual(create.min_replacement("MISSISSIPPI"), "ABCCBCCBDDB")
 
-    def words_present(self):
+    def test_unique_word(self):
+        self.assertEqual(create._unique_word("PROSPER"), "PROSE")
+
+    def test_words_present(self):
         self.assertTrue(len(self.words) > 10**4 * 4, 'wordlist error')
+
+if __name__ == '__main__':
+    unittest.main()
